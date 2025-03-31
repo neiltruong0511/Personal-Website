f383881facb26e83067d6e7a48493527fe8c486a
neiltruong <truongtu678@gmail.com>
2025-03-31 22:49:19 +0700

    First commit - Upload  Personal Website

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggleTheme").addEventListener("click", function () {
+    document.body.style.backgroundColor =
+        document.body.style.backgroundColor === "black" ? "#f5f5f5" : "black";
+});
